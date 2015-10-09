1715e05f95b8cab9cdb289b835dc5a34c9e83980
Philippe Mouchel <philippe.mouchel@savoirfairelinux.com>
2015-10-09 17:45:12 -0400

    Provide tow routes, one for home and one for admin

--- src/Controller/AdminSettingsController.php
+++ src/Controller/AdminSettingsController.php
@@ -1,20 +1,20 @@
 <?php
 /**
  * @file
- * Contains \Drupal\pokedex\Controller\HomeController.
+ * Contains \Drupal\pokedex\Controller\AdminSettingsController.
  */
 
 namespace Drupal\pokedex\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class HomeController extends ControllerBase {
+class AdminSettingsController extends ControllerBase {
 
   // Render content
   public function content() {
     return array(
         '#type' => 'markup',
-        '#markup' => t('Welcome to my D8 R&D module! This is a Pokedex.'),
+        '#markup' => t('Here you can configure your Pokedex.'),
     );
   }
 }
